a1c74de0aadfc5753ddc33f20bb5acb05d6eec29
samuel6378 <samuel202mwangi@gmail.com>
2024-04-09 14:16:57 +0300

    Responsiveness: modified the responsive of the inputs and background color

--- sem.js
+++ sem.js
@@ -1,34 +1,40 @@
   
 
-const inputContainer = document.querySelector('.form-fill');
-const enter = document.querySelector('.form-fill');
-const btns = document.querySelector('.link');
-const box = document.getElementById('form2Example3c');
-const takeValue = enter.value;
 
-const fNameInputs = document.querySelectorAll('.view');
+const btns = document.querySelector('.link');
 const show = document.querySelector('.verify');
 
-function load(){
-
-btns.addEventListener('click', () => {
-
-            show.classList.toggle("details");
-
-            setTimeout(() => {
-                show.classList.remove("details");
-              }, 3000);
-            input.value = '';        
-            }
-        )};
-  
 
 
-load();
+function validateForm() {
+  var fName = document.getElementById('fName').value;
+  var lName = document.getElementById('lName').value;
+  var email = document.getElementById('email').value;
+  var pass = document.getElementById('pass').value;
 
+  if (fName === '' || lName === '' || email === '' || pass === '') {
+    alert('Please fill in all fields');
+    return false;
+  }
+  return true;
+}
 
-    
+document.querySelector('.link').addEventListener('click', function(e) {
+  e.preventDefault();
+  if (validateForm()) {
+    btns.addEventListener('click', () => {
 
+      show.classList.toggle("details");
+
+      setTimeout(() => {
+          show.classList.remove("details");
+        }, 3000);
+           
+      }
+  )
+   
+  }
+});
 
 
 
